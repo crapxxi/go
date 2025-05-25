75615af32dad88e48da18122ec3835ce2ff07ccd
crapxxi <aibynkairatov@gmail.com>
2025-05-25 11:12:16 +0500

    okay

--- newbie-next/main.go
+++ newbie-next/main.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 	"os/signal"
+	"slices"
 	"strings"
 	"syscall"
 	"time"
@@ -64,75 +66,112 @@ func loadNotes(filename string) []Notes {
 }
 
 func main() {
-	file, err := os.Open("Notes.json")
-	fmt.Println(strings.Repeat("-", 12))
-	if os.IsNotExist(err) {
-		file, err = os.Create("Notes.json")
-		if err != nil {
-			printerror(err.Error())
-		}
-	}
-	defer file.Close()
-	notes := loadNotes("Notes.json")
-	for _, note := range notes {
-		fmt.Println(note.Header)
-		for _, line := range note.Text {
-			fmt.Println(line)
-		}
+	for {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		file, err := os.Open("Notes.json")
 		fmt.Println(strings.Repeat("-", 12))
-	}
+		if os.IsNotExist(err) {
+			file, err = os.Create("Notes.json")
+			if err != nil {
+				printerror(err.Error())
+			}
+		}
+		defer file.Close()
+		notes := loadNotes("Notes.json")
+		for i := 0; i < len(notes); i++ {
+			fmt.Printf("[%d]%s\n", i, notes[i].Header)
+			for _, line := range notes[i].Text {
+				fmt.Println(line)
+			}
+			fmt.Println(strings.Repeat("-", 12))
+		}
+		fmt.Println(len(notes))
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println(strings.Repeat("-", 12) + "\nNotes\n" + strings.Repeat("-", 12))
-	fmt.Println("[1]Create note\n[2]Edit note\n[3]Delete note\n[0]Exit")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println(strings.Repeat("-", 12) + "\nNotes\n" + strings.Repeat("-", 12))
+		fmt.Println("[1]Create note\n[2]Delete note\n[0]Exit")
 
-	var operation int
-	_, err = fmt.Scan(&operation)
-	if err != nil {
-		printerror(err.Error())
-		return
-	}
-	switch operation {
-	case 1:
-		fmt.Println("Please, enter header of the note:")
-		texth, err := reader.ReadString('\n')
+		var operation int
+		_, err = fmt.Scan(&operation)
 		if err != nil {
 			printerror(err.Error())
-			return
-		} else {
-			texth = strings.TrimSpace(texth)
-			fmt.Println("Please, enter the main text(Press CTRL+C when you're done):")
-			lines := []string{}
-			input := make(chan string)
+			continue
+		}
+		switch operation {
+		case 1:
+			fmt.Println("Please, enter header of the note:")
+			texth, err := reader.ReadString('\n')
+			if err != nil {
+				printerror(err.Error())
+				continue
+			} else {
+				texth = strings.TrimSpace(texth)
+				fmt.Println("Please, enter the main text(Press CTRL+C when you're done):")
+				lines := []string{}
+				input := make(chan string)
 
-			sigs := make(chan os.Signal, 1)
-			signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+				sigs := make(chan os.Signal, 1)
+				signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
-			go func() {
-				for {
-					textm, errm := reader.ReadString('\n')
-					if errm != nil {
-						break
+				go func() {
+					for {
+						textm, errm := reader.ReadString('\n')
+						if errm != nil {
+							break
+						}
+						textm = strings.TrimSpace(textm)
+						input <- textm
 					}
-					textm = strings.TrimSpace(textm)
-					input <- textm
+				}()
+			loop:
+				for {
+					select {
+					case line := <-input:
+						lines = append(lines, line)
+					case <-sigs:
+						break loop
+					}
 				}
-			}()
-		loop:
-			for {
-				select {
-				case line := <-input:
-					lines = append(lines, line)
-				case <-sigs:
-					break loop
-				}
-			}
 
-			thisnote := Notes{
-				Header: texth,
-				Text:   lines,
+				thisnote := Notes{
+					Header: texth,
+					Text:   lines,
+				}
+				saveNotes(thisnote, "Notes.json")
 			}
-			saveNotes(thisnote, "Notes.json")
+		case 2:
+			if len(notes) == 0 {
+				fmt.Println("Notes are empty!")
+				continue
+			}
+			fmt.Println("Please enter the index of the note: ")
+			var deletenote int
+			_, err = fmt.Scan(&deletenote)
+			if err != nil {
+				printerror(err.Error())
+				continue
+			}
+			if deletenote >= len(notes) || deletenote < 0 {
+				printerror("Incorrect index")
+				continue
+			}
+			notes = slices.Delete(notes, deletenote, deletenote+1)
+			data, err := json.MarshalIndent(notes, "", " ")
+			if err != nil {
+				printerror(err.Error())
+				continue
+			}
+			err = os.WriteFile("Notes.json", data, 0644)
+			if err != nil {
+				printerror(err.Error())
+				continue
+			}
+			fmt.Println("Deleted!")
+		case 0:
+			fmt.Println("Quiting program...")
+			return
 		}
 	}
 }
